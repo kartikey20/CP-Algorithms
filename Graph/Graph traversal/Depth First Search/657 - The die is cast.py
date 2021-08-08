588a55f727d8916690ef8b4796cb381095782cd9
Kartikey <ranjankartikey1999@gmail.com>
2021-08-08 19:11:40 +0530

    feat: :sparkles: Solved

--- Graph/Graph traversal/Depth First Search/657 - The die is cast.py
+++ Graph/Graph traversal/Depth First Search/657 - The die is cast.py
@@ -1,14 +1,14 @@
-def solve(w, h, board):
-    visited = [[False for _ in range(w)] for _ in range(h)]
+def solve(col, row, board):
+    visited = [[False for _ in range(col)] for _ in range(row)]
     visited[0][0] = True
 
-    def dfs(w, h, board, x, y, previousIsX, count, arr):
-        if x == w and y == h:
+    def dfs(endCol, endRow, board, col, row, previousIsX, count, arr):
+        if col == endCol and endRow == row:
             return count
         else:
-            for i in range(x - 1, y + 2):
-                for j in range(x - 1, y + 2):
-                    if visited[i][j] == False and i >= 0 and j >= 0 and i < w and j < h:
+            for i in range(row - 1, row + 2):
+                for j in range(col - 1, col + 2):
+                    if visited[i][j] == False and i >= 0 and j >= 0 and i < endRow and j < endCol:
                         visited[i][j] = True
                         if board[i][j] == 'X':
                             if not previousIsX:
@@ -16,13 +16,14 @@ def solve(w, h, board):
                             previousIsX = True
                         else:
                             previousIsX = False
-                        dfs(w, h, board, i, j, previousIsX, count, arr)
-    return dfs(w, h, board, 0, 0, False, 0, [])
+                        dfs(endCol, endRow, board, i,
+                            j, previousIsX, count, arr)
+    return dfs(col, row, board, 0, 0, False, 0, [])
 
 
-width, height = map(int, input().split())
+col, row = map(int, input().split())
 matrix = []
-for i in range(height):
+for i in range(row):
     matrix.append(list(input()))
 _ = map(int, input().split())
-print(solve(width, height, matrix))
+print(solve(col, row, matrix))
