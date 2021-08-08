38abb6b581285ab0b0f8f97f36e20d664e5aaf24
Kartikey <ranjankartikey1999@gmail.com>
2021-08-08 19:53:21 +0530

    feat: :sparkles: solved

--- Graph/Graph traversal/Depth First Search/657 - The die is cast.py
+++ Graph/Graph traversal/Depth First Search/657 - The die is cast.py
@@ -1,29 +1,29 @@
-def solve(col, row, board):
-    visited = [[False for _ in range(col)] for _ in range(row)]
+def solve(cols, rows, board, count):
+    visited = [[False for _ in range(cols)] for _ in range(rows)]
     visited[0][0] = True
 
-    def dfs(endCol, endRow, board, col, row, previousIsX, count, arr):
-        if col == endCol and endRow == row:
+    def dfs(x, y, previousIsX, count):
+        if x == cols and y == rows:
             return count
         else:
-            for i in range(row - 1, row + 2):
-                for j in range(col - 1, col + 2):
-                    if visited[i][j] == False and i >= 0 and j >= 0 and i < endRow and j < endCol:
-                        visited[i][j] = True
-                        if board[i][j] == 'X':
-                            if not previousIsX:
-                                count += 1
-                            previousIsX = True
-                        else:
-                            previousIsX = False
-                        dfs(endCol, endRow, board, i,
-                            j, previousIsX, count, arr)
-    return dfs(col, row, board, 0, 0, False, 0, [])
+            if visited[x][y] == False and x >= 0 and y >= 0 and x < rows and y < cols:
+                visited[x][y] = True
+                if board[x][y] == 'X':
+                    if not previousIsX:
+                        count += 1
+                    previousIsX = True
+                else:
+                    previousIsX = False
+            dfs(x + 1, y, previousIsX, count)
+            dfs(x - 1, y, previousIsX, count)
+            dfs(x, y + 1, previousIsX, count)
+            dfs(x, y - 1, previousIsX, count)
+    return dfs(0, 0, False)
 
 
-col, row = map(int, input().split())
+cols, rows = map(int, input().split())
 matrix = []
-for i in range(row):
+for i in range(rows):
     matrix.append(list(input()))
 _ = map(int, input().split())
-print(solve(col, row, matrix))
+print(solve(cols, rows, matrix))
