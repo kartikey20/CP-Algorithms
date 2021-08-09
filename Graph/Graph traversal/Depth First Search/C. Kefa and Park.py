6a65e961ae8f3b5aa3c43be8d446e7f25c5681b0
Kartikey <ranjankartikey1999@gmail.com>
2021-08-09 06:54:13 +0530

    feat: :art: Partially Working

--- Graph/Graph traversal/Depth First Search/C. Kefa and Park.py
+++ Graph/Graph traversal/Depth First Search/C. Kefa and Park.py
@@ -1,34 +1,29 @@
 # https://codeforces.com/problemset/problem/580/C
 # [[], [2, 3], [4, 5], [6, 7], [], [], [], []]
+# 0 1 0 1 1 0 0 0
 
-def dfs(graph, a, n, m, start=1):
-    stack = [start]
-    dp = [0] * (n + 1)
-    visited, finished = [False] * (n + 1), [False] * (n + 1)
 
-    while stack:
-        start = stack[-1]
+def solve(graph, hasCat, m):
+    n = len(graph)
+    visited = [False] * n
+    visited[1] = True
+    arr = []
+    count = hasCat[1]
 
-        # push unvisited children into stack
-        if not visited[start]:
-            visited[start] = True
-            for child in graph[start]:
-                if not visited[child]:
-                    stack.append(child)
-        else:
-            print(stack)
-            stack.pop()
-
-            # base case
-            dp[start] += a[start]
-
-            # update with finished children
-            for child in graph[start]:
-                if finished[child]:
-                    dp[start] += dp[child]
-
-            finished[start] = True
-    return dp
+    def dfs(node, count):
+        if graph[node] == []:
+            arr.append(node)
+        for child in graph[node]:
+            if not visited[child]:
+                visited[child] = True
+                if hasCat[child] == 1:
+                    count += 1
+                else:
+                    count = 0
+                if count <= m:
+                    dfs(child, count)
+    dfs(1, count)
+    return arr
 
 
 n, m = map(int, input().split())
@@ -38,4 +33,4 @@ graph = [[] for _ in range(n + 1)]
 for _ in range(n - 1):
     x, y = map(int, input().split())
     graph[x].append(y)
-print(dfs(graph, a, n, m))
+print(solve(graph, a, m))
--- Graph/Graph traversal/Depth First Search/KOZE - Sheep.py
+++ Graph/Graph traversal/Depth First Search/KOZE - Sheep.py
@@ -1,4 +1,38 @@
+# [
+# ['.######.'],
+# ['#..k...#'],
+# ['#.####.#'],
+# ['#.#v.#.#'],
+# ['#.#.k#k#'],
+# ['#k.##..#'],
+# ['#.v..v.#'],
+# ['.######.']
+# ]
+
+def solve(n, m, matrix):
+    sheep = 0
+    wolf = 0
+    totalSheep = 0
+    totalWolf = 0
+
+    def dfs(i, j):
+
+        return
+
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] != '#':
+                dfs(i, j)
+                if sheep > wolf:
+                    totalWolf += wolf
+                else:
+                    totalSheep += sheep
+
+    return
+
+
 N, M = map(int, input().split())
 matrix = []
 for _ in range(N):
-    matrix.append(list(map(int, input().split())))
+    matrix.append(list(input().split()))
+print(solve(N, M, matrix))
