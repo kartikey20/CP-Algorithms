44b9eca8008deff7940b51d3e3f996536ac9058a
Kartikey <ranjankartikey1999@gmail.com>
2021-08-10 00:06:18 +0530

    feat: :sparkles: Solved

--- Graph/Graph traversal/Depth First Search/KOZE - Sheep.py
+++ Graph/Graph traversal/Depth First Search/KOZE - Sheep.py
@@ -8,31 +8,53 @@
 # ['#.v..v.#'],
 # ['.######.']
 # ]
+def valid(x, y, N, M):
+    if x >= 0 and x < N and y >= 0 and y < M:
+        return True
+    return False
 
-def solve(n, m, matrix):
-    sheep = 0
-    wolf = 0
-    totalSheep = 0
-    totalWolf = 0
 
-    def dfs(i, j):
+def dfs(x, y, visited, matrix, k, v):
+    visited[x][y] = True
+    if matrix[x][y] == 'k':
+        k += 1
+    if matrix[x][y] == 'v':
+        v += 1
+    if valid(x + 1, y, N, M) and visited[x + 1][y] == False and matrix[x + 1][y] != '#':
+        return dfs(x + 1, y, visited, matrix, k, v)
+    if valid(x - 1, y, N, M) and visited[x - 1][y] == False and matrix[x - 1][y] != '#':
+        return dfs(x - 1, y, visited, matrix, k, v)
+    if valid(x, y + 1, N, M) and visited[x][y + 1] == False and matrix[x][y + 1] != '#':
+        return dfs(x, y + 1, visited, matrix, k, v)
+    if valid(x, y - 1, N, M) and visited[x][y - 1] == False and matrix[x][y - 1] != '#':
+        return dfs(x, y - 1, visited, matrix, k, v)
+    return k, v, visited
 
-        return
 
-    for i in range(n):
-        for j in range(m):
-            if matrix[i][j] != '#':
-                dfs(i, j)
-                if sheep > wolf:
-                    totalWolf += wolf
+def solve(N, M, matrix, sheep, wolves):
+    visited = [[False for _ in range(M)] for _ in range(N)]
+    for i in range(N):
+        for j in range(M):
+            if matrix[i][j] == 'k' and visited[i][j] == False:
+                k, v = 0, 0
+                k, v, visited = dfs(i, j, visited, matrix, k, v)
+                if k > v:
+                    wolves -= v
                 else:
-                    totalSheep += sheep
-
-    return
+                    sheep -= k
+    return wolves, sheep
 
 
 N, M = map(int, input().split())
 matrix = []
+wolves, sheep = 0, 0
 for _ in range(N):
-    matrix.append(list(input().split()))
-print(solve(N, M, matrix))
+    temp = input()
+    for i in temp:
+        if i == 'k':
+            sheep += 1
+        if i == 'v':
+            wolves += 1
+    matrix.append(temp)
+print(solve(N, M, matrix, sheep, wolves))
+# print(sheep, wolves)
