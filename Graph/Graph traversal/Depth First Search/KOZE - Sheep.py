71433c111abec9fbea75b6f122e5d10042acee2a
Kartikey <ranjankartikey1999@gmail.com>
2021-08-10 05:43:24 +0530

    feat: :sparkles: solved

--- Graph/Graph traversal/Depth First Search/KOZE - Sheep.py
+++ Graph/Graph traversal/Depth First Search/KOZE - Sheep.py
@@ -14,21 +14,23 @@ def valid(x, y, N, M):
     return False
 
 
-def dfs(x, y, visited, matrix, k, v):
+k, v = [-1], [-1]
+
+
+def dfs(x, y, visited, matrix):
     visited[x][y] = True
     if matrix[x][y] == 'k':
-        k += 1
+        k[0] += 1
     if matrix[x][y] == 'v':
-        v += 1
+        v[0] += 1
     if valid(x + 1, y, N, M) and visited[x + 1][y] == False and matrix[x + 1][y] != '#':
-        return dfs(x + 1, y, visited, matrix, k, v)
+        dfs(x + 1, y, visited, matrix)
     if valid(x - 1, y, N, M) and visited[x - 1][y] == False and matrix[x - 1][y] != '#':
-        return dfs(x - 1, y, visited, matrix, k, v)
+        dfs(x - 1, y, visited, matrix)
     if valid(x, y + 1, N, M) and visited[x][y + 1] == False and matrix[x][y + 1] != '#':
-        return dfs(x, y + 1, visited, matrix, k, v)
+        dfs(x, y + 1, visited, matrix)
     if valid(x, y - 1, N, M) and visited[x][y - 1] == False and matrix[x][y - 1] != '#':
-        return dfs(x, y - 1, visited, matrix, k, v)
-    return k, v, visited
+        dfs(x, y - 1, visited, matrix)
 
 
 def solve(N, M, matrix, sheep, wolves):
@@ -36,13 +38,14 @@ def solve(N, M, matrix, sheep, wolves):
     for i in range(N):
         for j in range(M):
             if matrix[i][j] == 'k' and visited[i][j] == False:
-                k, v = 0, 0
-                k, v, visited = dfs(i, j, visited, matrix, k, v)
-                if k > v:
-                    wolves -= v
+                k[0] = 0
+                v[0] = 0
+                dfs(i, j, visited, matrix)
+                if k[0] > v[0]:
+                    wolves -= v[0]
                 else:
-                    sheep -= k
-    return wolves, sheep
+                    sheep -= k[0]
+    return " ".join(map(str, (sheep, wolves)))
 
 
 N, M = map(int, input().split())
---
+++ Graph/Graph traversal/Depth First Search/MAKEMAZE - VALIDATE THE MAZE.py
@@ -0,0 +1,44 @@
+def validate(matrix, m, n):
+    visited = [[False for _ in range(n)] for _ in range(m)]
+    points = []
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j] == '.':
+                if j == 0 or j == n - 1:
+                    points.append([i, j])
+                if i == 0 or i == m - 1:
+                    points.append([i, j])
+    if len(points) <= 1:
+        return "invalid"
+
+    def valid(x, y):
+        if x >= 0 and x < m and y >= 0 and y < n:
+            return True
+        return False
+
+    def dfs(x, y):
+        visited[x][y] = True
+        if valid(x + 1, y) and visited[x + 1][y] and matrix[x + 1][y] != '#':
+            dfs(x + 1, y)
+        if valid(x - 1, y) and visited[x - 1][y] and matrix[x - 1][y] != '#':
+            dfs(x - 1, y)
+        if valid(x, y - 1) and visited[x][y - 1] and matrix[x][y - 1] != '#':
+            dfs(x, y - 1)
+        if valid(x, y + 1) and visited[x][y + 1] and matrix[x][y + 1] != "#":
+            dfs(x, y + 1)
+
+    dfs(points[0][0], points[0][1])
+
+    if any(visited[i[0]][i[1]] for i in points[1:]):
+        return "valid"
+
+    return "invalid"
+
+
+T = int(input())
+res = []
+for _ in range(T):
+    m, n = map(int, input().split())
+    matrix = [input() for i in range(m)]
+    res.append(validate(matrix, m, n))
+print(validate(matrix, m, n))
