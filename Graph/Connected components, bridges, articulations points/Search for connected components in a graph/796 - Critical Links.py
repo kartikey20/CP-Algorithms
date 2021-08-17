c90cd1bf60c798f49b59141273fda1e526d31c15
Kartikey <ranjankartikey1999@gmail.com>
2021-08-17 18:46:05 +0530

    fix: :ambulance: solved

---
+++ Graph/Connected components, bridges, articulations points/Search for connected components in a graph/796 - Critical Links.py
@@ -0,0 +1,14 @@
+def solve():
+    def dfs():
+        return
+    return
+
+
+noOfServers = int(input())
+for _ in range(noOfServers):
+    noOfConnections = int(input())
+    connections = []
+    for _ in range(noOfConnections):
+        connections.append(list(map(int, input().split())))
+    print(connections)
+    print(dfs())
